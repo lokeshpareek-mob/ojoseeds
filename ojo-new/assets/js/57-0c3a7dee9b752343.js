190c9c00162d0118f82d11f37b70c6bab405abd9
lokeshpareek-mob <142871034+lokeshpareek-mob@users.noreply.github.com>
2025-03-31 21:04:08 +0530

    Add files via upload

---
+++ ojo-new/assets/js/57-0c3a7dee9b752343.js
@@ -0,0 +1,114 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [57], {
+        2118: function(e, s, a) {
+            a.d(s, { d: function() { return l } });
+            var r = a(5893),
+                n = a(8434),
+                t = a(4141),
+                l = function() { return (0, r.jsx)(n.Z, { className: { position: "fixed", bottom: "30px", right: "20px" }, children: (0, r.jsx)("a", { href: "https://wa.me/917877151455?text=", target: "_blank", children: (0, r.jsx)(t.Z, { src: "./WhatsApp.png", alt: "chat on whatsapp", height: 60, width: 240 }) }) }) }
+        },
+        7066: function(e, s, a) {
+            a.d(s, { h: function() { return Z } });
+            var r = a(603),
+                n = a(5893),
+                t = a(1664),
+                l = a.n(t),
+                c = a(7294),
+                i = a(8434),
+                o = a(4141),
+                h = a(516),
+                u = a(2712),
+                x = function() {
+                    var e = (0, c.useState)(!1),
+                        s = e[0],
+                        a = e[1],
+                        r = (0, c.useCallback)(function() { return a(!0) }, []),
+                        n = (0, c.useCallback)(function() { return a(!1) }, []);
+                    return [{ onMouseEnter: r, onMouseLeave: n }, s]
+                },
+                d = a(4979),
+                f = a(161),
+                j = function(e) {
+                    var s = e.label,
+                        a = (0, r.Z)(x(), 2),
+                        t = a[0],
+                        l = t.onMouseEnter,
+                        c = t.onMouseLeave,
+                        o = a[1];
+                    return (0, n.jsxs)(i.Z, { onMouseEnter: l, onMouseLeave: c, onFocus: l, onBlur: c, children: [(0, n.jsx)(u.Y, { className: "px-2 py-1", children: (0, n.jsx)(h.Z, { variant: "h4", weight: "regular", children: s }) }), (0, n.jsx)(i.Z, { className: [{ height: "2px", backgroundColor: f.iZ }, "opacity-0 transition-all transform duration-200 scale-0 ease-in-ease-out", o ? "opacity-100 scale-100" : "", ] })] })
+                },
+                p = function() { return (0, n.jsxs)(i.Z, { className: "flex items-center gap-3", children: [(0, n.jsx)(l(), { href: "/", children: (0, n.jsx)("div", { children: (0, n.jsx)(j, { label: "Home" }) }) }), (0, n.jsx)(l(), { href: "./about-us", children: (0, n.jsx)("div", { children: (0, n.jsx)(j, { label: "About Us" }) }) }), (0, n.jsx)(l(), { href: "/research-development", children: (0, n.jsx)("div", { children: (0, n.jsx)(j, { label: "Research" }) }) }), (0, n.jsx)(l(), { href: "/sales", children: (0, n.jsx)("div", { children: (0, n.jsx)(j, { label: "Sales" }) }) }), (0, n.jsx)(l(), { href: "/contact-us", children: (0, n.jsx)("div", { children: (0, n.jsx)(j, { label: "Contact Us" }) }) })] }) },
+                m = function() {
+                    var e = (0, c.useState)(!1),
+                        s = e[0],
+                        a = e[1];
+                    return (0, n.jsxs)(n.Fragment, { children: [(0, n.jsx)(u.Y, { className: "hover:spr-ui-focus", onClick: function() { return a(function(e) { return !e }) }, children: (0, n.jsx)(o.Z, { src: "/stack.png", alt: "nav icon" }) }), (0, n.jsx)(i.Z, { className: ["spr-ui-02", "transition-all transform duration-200 ease-in-ease-out", { position: "fixed", top: 0, left: 0, bottom: 0, width: "250px", zIndex: "100", transform: "translateX(-250px)" }, s ? { transform: "translateX(0)" } : "", ], children: (0, n.jsxs)(i.Z, { className: "flex flex-col gap-3 pt-4 px-4", children: [(0, n.jsx)(l(), { href: "/", children: (0, n.jsx)(u.Y, { className: "hover:spr-ui-focus rounded-4 p-1 justify-start", children: (0, n.jsx)(h.Z, { variant: "h4", weight: "regular", className: "spr-text-01", children: "Home" }) }) }), (0, n.jsx)(l(), { href: "./about-us", children: (0, n.jsx)(u.Y, { className: "hover:spr-ui-focus rounded-4 p-1 justify-start", children: (0, n.jsx)(h.Z, { variant: "h4", weight: "regular", className: "spr-text-01", children: "About Us" }) }) }), (0, n.jsx)(l(), { href: "/research-development", children: (0, n.jsx)(u.Y, { className: "hover:spr-ui-focus rounded-4 p-1 justify-start", children: (0, n.jsx)(h.Z, { variant: "h4", weight: "regular", className: "spr-text-01", children: "Research" }) }) }), (0, n.jsx)(l(), { href: "/sales", children: (0, n.jsx)(u.Y, { className: "hover:spr-ui-focus rounded-4 p-1 justify-start", children: (0, n.jsx)(h.Z, { variant: "h4", weight: "regular", className: "spr-text-01", children: "Sales" }) }) }), (0, n.jsx)(l(), { href: "/contact-us", children: (0, n.jsx)(u.Y, { className: "hover:spr-ui-focus rounded-4 p-1 justify-start", children: (0, n.jsx)(h.Z, { variant: "h4", weight: "regular", className: "spr-text-01", children: "Contact Us" }) }) })] }) })] })
+                },
+                Z = function() { var e = (0, d.n)().md; return (0, n.jsx)(i.Z, { className: ["w-full spr-ui-01", { height: "72px" }], children: (0, n.jsxs)(i.Z, { className: ["flex items-center mx-auto h-full", f.ZG], children: [(0, n.jsxs)(i.Z, { className: "flex items-center gap-3", children: [(0, n.jsx)(o.Z, { src: "./logo.png", height: 50, width: 50, alt: "logo" }), (0, n.jsx)(h.Z, { variant: "h1", className: [{ color: f.iZ }], children: "OJO Seeds" })] }), (0, n.jsx)(i.Z, { className: "flex-1" }), e ? (0, n.jsx)(p, {}) : (0, n.jsx)(m, {})] }) }) }
+        },
+        3276: function(e, s, a) {
+            a.d(s, { PN: function() { return n }, Ug: function() { return t }, aP: function() { return l }, o4: function() { return r } });
+            var r = "Ojo Seeds Pvt. Ltd., Vokkaleri, Kolar, Karnataka, India, Pin Code: 563130",
+                n = "ojoseeds@gmail.com",
+                t = "+91 78771 51455",
+                l = [{ src: "https://www.youtube.com/watch?v=BFukVGpZL3o", label: "Testimonial 1" }, { src: "https://www.youtube.com/watch?v=BFukVGpZL3o", label: "Testimonial 2" }, { src: "https://www.youtube.com/watch?v=BFukVGpZL3o", label: "Testimonial 3" }, { src: "https://www.youtube.com/watch?v=BFukVGpZL3o", label: "Testimonial 4" }]
+        },
+        6032: function(e, s, a) {
+            a.d(s, { $: function() { return u } });
+            var r = a(5893),
+                n = a(8434),
+                t = a(4141),
+                l = a(516),
+                c = a(6775),
+                i = a(4979),
+                o = a(161),
+                h = a(3276),
+                u = function() {
+                    var e = (0, i.n)().sm;
+                    return (0, r.jsx)(n.Z, {
+                        className: ["w-full py-16", { backgroundColor: "#1c232c", maxHeight: "500px" }, ],
+                        children: (0, r.jsx)(n.Z, {
+                            className: [o.ZG, "mx-auto h-full"],
+                            children: (0, r.jsxs)(n.Z, {
+                                className: ["flex gap-5", e ? "" : "flex-col"],
+                                children: [(0, r.jsxs)(n.Z, { className: "min-w-0 flex-1 flex flex-col gap-3", children: [(0, r.jsx)(l.Z, { variant: "h3", className: "pb-2", children: "Corporate Office" }), (0, r.jsxs)(n.Z, { className: "flex gap-2", children: [(0, r.jsx)(n.Z, { className: "flex-shrink-0", children: (0, r.jsx)(t.Z, { src: "./location.png", height: 24, width: 24, alt: "location icon" }) }), (0, r.jsxs)(l.Z, { variant: "bl2", className: { color: "#bfbfbf" }, children: [(0, r.jsx)("b", { children: "Ojo Seeds Pvt. Ltd." }), " ", (0, r.jsx)("br", {}), "Vakkaleri, Kolar, Karnataka, India, Pin Code: 563130"] })] }), (0, r.jsxs)(n.Z, { className: "flex gap-2", children: [(0, r.jsx)(n.Z, { className: "flex-shrink-0", children: (0, r.jsx)(t.Z, { src: "./email.png", height: 24, width: 24, alt: "email icon" }) }), (0, r.jsx)(l.Z, { variant: "bl2", className: { color: "#bfbfbf" }, children: h.PN })] }), (0, r.jsxs)(n.Z, { className: "flex gap-2", children: [(0, r.jsx)(n.Z, { className: "flex-shrink-0", children: (0, r.jsx)(t.Z, { src: "./phone.png", height: 24, width: 24, alt: "phone icon" }) }), (0, r.jsx)(l.Z, { variant: "bl2", className: { color: "#bfbfbf" }, children: h.Ug })] })] }), (0, r.jsxs)(n.Z, {
+                                    className: "min-w-0 flex-1 flex flex-col gap-2",
+                                    children: [(0, r.jsx)(l.Z, { variant: "h3", className: "pb-3 w-full", children: "Testimonials" }), (0, r.jsx)(n.Z, {
+                                        className: "w-full flex flex-col gap-2 flex-wrap",
+                                        children: h.aP.map(function(e) {
+                                            var s = e.src,
+                                                a = e.label;
+                                            return (0, r.jsxs)(c.Z, { href: s, className: [{ color: "#bfbfbf" }, { ":hover": { color: o.iZ } }, "hover:no-underline", ], target: "_blank", children: ["> ", a] }, s)
+                                        })
+                                    })]
+                                })]
+                            })
+                        })
+                    })
+                }
+        },
+        161: function(e, s, a) {
+            a.d(s, { Vz: function() { return t }, ZG: function() { return r }, iZ: function() { return n } });
+            var r = { maxWidth: "1200px", paddingLeft: "12px", paddingRight: "12px" },
+                n = "#246338",
+                t = "#bad700"
+        },
+        4979: function(e, s, a) {
+            a.d(s, { n: function() { return u } });
+            var r = a(4924),
+                n = a(6042),
+                t = a(9396),
+                l = a(7294),
+                c = a(6018),
+                i = a(4168),
+                o = a(4061),
+                h = a.n(o),
+                u = function() {
+                    var e = (0, c.Z)().width,
+                        s = (0, i.Z)().theme.screens;
+                    return (0, l.useMemo)(function() { return h()(s, function(s, a, l) { return (0, t.Z)((0, n.Z)({}, s), (0, r.Z)({}, l, e > parseInt(a, 10))) }, {}) }, [s, e])
+                }
+        }
+    }
+]);
---
+++ ojo-new/assets/js/index-37bd044ef2f4866f.js
@@ -0,0 +1,64 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [405], {
+        8312: function(e, r, s) {
+            (window.__NEXT_P = window.__NEXT_P || []).push(["/", function() { return s(7475) }])
+        },
+        7475: function(e, r, s) {
+            "use strict";
+            s.r(r), s.d(r, { default: function() { return _ } });
+            var i, n, l = s(5893),
+                a = s(9008),
+                t = s.n(a),
+                o = s(5152),
+                c = s.n(o),
+                d = s(7066),
+                u = s(8434),
+                h = s(516),
+                f = s(4141),
+                x = s(161),
+                m = s(4979),
+                p = { Root: { style: "h-full" }, Image: { style: { objectFit: "contain" } } },
+                g = function(e) {
+                    var r = e.products,
+                        s = (0, m.n)(),
+                        i = s.xs,
+                        n = s.lg;
+                    return (0, l.jsx)(u.Z, {
+                        className: "w-full flex gap-4 flex-wrap",
+                        children: r.map(function(e) {
+                            var r = e.id,
+                                s = e.name,
+                                a = e.usp,
+                                t = e.src;
+                            return (0, l.jsx)(u.Z, { className: [n ? { flex: "1 0 45%", maxWidth: "50%" } : { flex: "1 0 100%", maxWidth: "100%" }, "pb-5", ], children: (0, l.jsxs)(u.Z, { className: ["flex gap-2 spr-border-02 border-solid border-1 rounded-4 overflow-hidden", i ? "" : "flex-col", i ? { height: "300px" } : "", ], children: [(0, l.jsx)(u.Z, { className: { flex: "0 0 40%" }, children: (0, l.jsx)(f.Z, { src: t, alt: s, overrides: p }) }), (0, l.jsxs)(u.Z, { className: ["flex flex-col flex-1 gap-2", i ? "" : "ml-3"], children: [(0, l.jsxs)(u.Z, { className: "flex flex-col", children: [(0, l.jsx)(h.Z, { variant: "h4", className: "spr-text-01", weight: "bold", children: s }), (0, l.jsx)(u.Z, { className: [{ height: "2px", width: "70px", backgroundColor: x.iZ }, ] })] }), (0, l.jsx)(u.Z, { className: "flex flex-col gap-1 pb-3", children: a.map(function(e) { return (0, l.jsx)(h.Z, { variant: "bl1", className: ["spr-text-02 ml-4", { display: "list-item", listStyleType: "square" }, ], children: e }, e) }) })] })] }) }, r)
+                        })
+                    })
+                };
+            (n = i || (i = {})).ALL = "ALL", n.VEGETABLES = "VEGETABLES", n.FRUITS = "FRUITS", i.ALL, i.FRUITS, i.VEGETABLES;
+            var Z = [{ id: "ridgeGourd", name: "Cucumber", src: "./ridgeGuord.jpg", usp: ["Thick trunk, large leaves", "Dark green shining fruit", "First harvest in 30-40 days", "Fruit weight: 150-190 grams", "Fruit length: 18-22cm", "More disease tolerant", "Multi fruit on every node", "Re-fruiting from starting", "Long life duration plant", "No fruit deaths due to fast fruit cycle"] }, { id: "chilliJacqueline", name: "Chilli - Gori", src: "./chilli.jpg", usp: ["Big plant, dark green leaves", "Early maturity", "High disease tolerant", "Ample production", "Dark green shining", "Length: 16-18cm", "Diameter: 2.5 cm", "Good for transport"] }, { id: "chilliEnglish", name: "Chilli - Anjali", src: "./chilli-long.jpg", usp: ["Big plant, dark green leaves", "Early maturity", "High disease tolerant", "Ample production", "Pencil type fruit", "Dark green shining", "Length: 20-22 cm", "Diameter: 1.5 cm", "Good for transport", ] }, { id: "waterMelon", name: "Water Melon", src: "./watermelon.jpg", usp: ["Thick trunk, large leaves", "Strong vine", "First harvest in 65-70 days", "Early harvest and stay for longer duration", "Oval shaped fruit, high sweetness", "Black shining skin", "Fruit weight: 4-5 kg", "Good for transport and store", "Good fruit quality", "Ample production", ] }, ],
+                j = function() { return (0, l.jsx)(u.Z, { className: "w-full spr-ui-01 py-16", children: (0, l.jsxs)(u.Z, { className: ["flex flex-col gap-8 items-center mx-auto", x.ZG, ], children: [(0, l.jsx)(h.Z, { variant: "h2", weight: "bold", className: ["text-40", { color: x.iZ }], children: "Our Products" }), (0, l.jsx)(u.Z, { className: "flex gap-6", children: (0, l.jsx)(g, { products: Z }) })] }) }) },
+                v = s(1664),
+                b = s.n(v),
+                N = s(6042),
+                y = s(9396),
+                w = s(9534),
+                k = s(3256),
+                E = function(e) { return { borderTopColor: e, borderLeftColor: e, borderBottomColor: e, borderRightColor: e } },
+                C = function(e) {
+                    var r = e.children,
+                        s = e.variant,
+                        i = void 0 === s ? "primary" : s,
+                        n = e.isSelected,
+                        a = (0, w.Z)(e, ["children", "variant", "isSelected"]);
+                    return (0, l.jsx)(k.Z, (0, y.Z)((0, N.Z)({}, a), { variant: i, isSelected: n, className: "primary" === i ? [(0, N.Z)({ backgroundColor: x.iZ }, E(x.iZ)), { ":hover": (0, N.Z)({ backgroundColor: x.Vz }, E(x.Vz)) }, a.className, ] : [(0, y.Z)((0, N.Z)({}, E(x.iZ)), { color: x.iZ }), { ":hover": (0, N.Z)({ color: "#fff", backgroundColor: x.iZ }, E(x.iZ)) }, n ? (0, N.Z)({ color: "#fff", backgroundColor: x.iZ }, E(x.iZ)) : {}, ], children: r }))
+                },
+                L = function() { var e = (0, m.n)().sm; return (0, l.jsx)(u.Z, { className: "spr-ui-05 w-full py-16", children: (0, l.jsx)(u.Z, { className: [x.ZG, "mx-auto flex gap-6", e ? "" : "flex-col items-center", ], children: (0, l.jsxs)(u.Z, { className: ["flex flex-col gap-6 min-w-0 flex-1", { flexGrow: 2 }, e ? "" : "items-center", ], children: [(0, l.jsx)(h.Z, { variant: "h1", className: "spr-text-02 font-300", children: "About Us" }), (0, l.jsxs)(h.Z, { variant: "h5", className: "spr-text-03 font-400", children: [(0, l.jsx)("b", { children: "Ojo Seeds Private Limited" }), " is a leading seed company serves mainly in the vegetable sector in India. Our honest and certified partner scientists, partner breeders, and partner researchers bring to you the best quality of seeds at reasonable rates. We are sincerely serving the needs of thousands of Indian farmers with honesty and integrity."] }), (0, l.jsx)(u.Z, { className: "pt-4", children: (0, l.jsx)(b(), { href: "/about-us.html", children: (0, l.jsx)(C, { shape: "pill", size: "lg", children: "Know More" }) }) })] }) }) }) },
+                G = s(6032),
+                S = s(2118);
+
+            function T() { return (0, l.jsxs)(u.Z, { className: "w-full h-full", children: [(0, l.jsxs)(t(), { children: [(0, l.jsx)("link", { rel: "icon", href: "/favicon.ico" }), (0, l.jsx)("title", { children: "Ojo Seeds" })] }), (0, l.jsx)(d.h, {}), (0, l.jsx)(j, {}), (0, l.jsx)(L, {}), (0, l.jsx)(G.$, {}), (0, l.jsx)(S.d, {})] }) }
+            var _ = c()(function() { return Promise.resolve(T) }, { ssr: !1 })
+        }
+    },
+    function(e) { e.O(0, [532, 753, 57, 774, 888, 179], function() { return e(e.s = 8312) }), _N_E = e.O() }
+]);
